941eca6503f76e592e10a4afe65d4af1fd76138a
amratansh12 <ashri1205@gmail.com>
2024-06-19 21:33:31 +0530

    Adding transactions api and features

---
+++ features/transactions/api/use-edit-transaction.ts
@@ -0,0 +1,40 @@
+import { toast } from "sonner";
+
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.transactions)[":id"]["$patch"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.transactions)[":id"]["$patch"]
+>["json"];
+
+export const useEditTransaction = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.transactions[":id"].$patch({
+        param: { id },
+        json,
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Transaction updated");
+      queryClient.invalidateQueries({
+        //TODO: Check if this {accountId: id} method works
+        queryKey: ["transaction", { accountId: id }],
+      });
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+    },
+    onError: () => {
+      toast.error("Failed to edit transaction");
+    },
+  });
+
+  return mutation;
+};
